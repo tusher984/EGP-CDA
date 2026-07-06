5ec275cd47870ae3d85de047824047dd378625f1
Tusher <tusher@netra.news>
2026-07-06 13:00:46 +0600

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+// data.js
+const cdaData = [
+  // ... PASTE YOUR ENTIRE JSON ARRAY HERE ...
+  {
+    "Tender/Proposal Package Name": "Construction of Road along the bank of Karnafully River...",
+    "Contract Value (Taka)": "8812446857",
+    "Procurement Method": "OTM"
+  }
+];
